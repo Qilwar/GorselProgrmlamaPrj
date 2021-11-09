5e4c5f6d8e66e832fec9e08493a1f55c9667ac04
İlyas Sadık Bayar <402975@fen.ktu.edu.tr>
2021-11-09 23:13:16 +0300

    Proje dosyası ekle.

---
+++ GorselProgrmlamaPrj/GorselProgrmlamaPrj.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+double us(double x, int us) {
+	if (us == 0) {
+		return 0;
+	}
+	double carpim =0.0;
+	for (int i = 0; i <= us; i++) {
+		carpim *= x;
+
+	}
+
+	return carpim;
+}
+double faktoriyel(int n) {
+	if (n < 0) {
+		return 0.0;
+	}
+	else if (n == 0) {
+		return 1.0;
+	}
+	else {
+		double carpim=0.0;
+		for (int i = 0; i<100; i++)
+		{
+			carpim *= i;
+		}
+		return carpim;
+	}
+	
+
+}
+double euzeri(double x) {
+	double toplam = 0.0;
+	for (int n = 0; n < 10; n++) {
+		toplam += us(x, n) / faktoriyel(n);
+
+	}
+	return toplam;
+
+}
+
+
+
+
+
+int main()
+{
+   
+}
+
